4d3fcd0718c382e74c709087a3624a77941ca299
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-09-24 13:45:04 -0700

    more examples

---
+++ examples/file-streaming/README.md
@@ -0,0 +1,22 @@
+# Overview
+Given a text file, convert all the characters into uppercase.
+
+input.txt
+```
+this text is all lowercase. please turn it into to uppercase case.
+```
+
+oupput.txt
+```
+THIS TEXT IS ALL LOWERCASE. PLEASE TURN IT INTO TO UPPERCASE CASE.
+```
+
+# How to run 
+
+Simply type the following commands. We assume you have nodejs and yarn installed.
+
+```
+yarn install
+yarn start input.txt output.txt
+```
+
--- examples/file-streaming/index.js
+++ examples/file-streaming/index.js
@@ -1,4 +1,8 @@
-const { print, utf8, FileStreamIn, FileStreamOut} = require('lesscode-fp')
+const { $M, print, utf8, FileStreamIn, FileStreamOut} = require('lesscode-fp')
 
-FileStreamIn(utf8)(FileStreamOut(utf8)('output.txt'))('input.txt').catch(print)
+const Uppercase = async str => str.toUpperCase()
+const SaveFile = name => FileStreamOut(utf8)(name)
+
+// Pipeline
+FileStreamIn(utf8)($M(SaveFile(process.argv[3]),Uppercase))(process.argv[2]).catch(print)
   

b66e3bd8e010c60f45f2d9fa514e4d1a994e4522
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-09-30 19:00:06 -0700

    readme update

--- lesscode/examples/image-download/index.js
+++ lesscode/examples/image-download/index.js
@@ -7,33 +7,21 @@ const {
     FileRead, FileWrite,
     HttpGET } = require('lesscode-fp')
 
+const inputFile = process.argv[2]
+const outputFile = process.argv[3]
+
 // processFile :: String -> String
 const ProcessURL = name => {
     const computeHash = $(hash('sha256'), mget('data'))
     const contentLen = mgettwo('headers')('content-length')
     const LogData = name => async data => `${name} ${contentLen(data)} ${computeHash(data)}`
     const LogErorr = name => async err => `${name} 0  ${escape(err)}`
-    return $M(
-        trace(`Extracted metadata.............`), LogData(name),      // Success
-        hint(`Downloaded ${name}..............`), HttpGET)(name)
-        .catch($(trace(`[Fail] : ${name}......`), LogErorr(name)))    // Failure
+    return $M(LogData(name), HttpGET)(name).catch(LogErorr(name))
 }
 
 // ProcessContent :: String -> String
-const ProcessContent = input => 
-    $M(
-        trace('Convert List 2 String..........'), l2String(newline),
-        trace('Waited.........................'), Wait,
-        trace('Processed URLs.................'), lmap(ProcessURL),
-        trace('Converted to List..............'), s2List(linebreak))(input)
-
-const inputFile = process.argv[2]
-const outputFile = process.argv[3]
+const ProcessContent = $M(l2String(newline), Wait, lmap(ProcessURL), s2List(linebreak))
 
 // Main pipeline.
-$M(
-    trace('Wrote to output file................'), FileWrite(utf8)(outputFile),
-    trace('Processed content...................'), ProcessContent,
-    trace('Read input file.....................'), FileRead(utf8)
-)(inputFile)
+$M(FileWrite(utf8)(outputFile), ProcessContent, FileRead(utf8))(inputFile)
 .catch(print)

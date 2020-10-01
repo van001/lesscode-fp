19fff4195dc3a6cf0f5084051aab3f638f5d6c82
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-09-30 18:53:35 -0700

    readme update

--- lesscode/examples/image-download/index.js
+++ lesscode/examples/image-download/index.js
@@ -7,31 +7,35 @@ const {
     FileRead, FileWrite,
     HttpGET } = require('lesscode-fp')
 
-const inputFile = process.argv[2]
-const outputFile = process.argv[3]
+
 
 // processFile :: String -> String
-const processURL = name => {
+const ProcessURL = name => {
     const computeHash = $(hash('sha256'), mget('data'))
     const contentLen = mgettwo('headers')('content-length')
     const LogData = name => async data => `${name} ${contentLen(data)} ${computeHash(data)}`
     const LogErorr = name => async err => `${name} 0  ${escape(err)}`
     return $M(
-        trace(`Extracted metadata...............`), LogData(name),      // Success
-        hint(`Downloaded ${name}................`), HttpGET)(name)
-        .catch($(trace(`[Fail] : ${name}........`), LogErorr(name)))    // Failure
+        trace(`Extracted metadata.............`), LogData(name),      // Success
+        hint(`Downloaded ${name}..............`), HttpGET)(name)
+        .catch($(trace(`[Fail] : ${name}......`), LogErorr(name)))    // Failure
 }
 
-// Pipeline
+// ProcessContent :: String -> String
+const ProcessContent = input => 
+    $M(
+        trace('Convert List 2 String..........'), l2String(newline),
+        trace('Waited.........................'), Wait,
+        trace('Processed URLs.................'), lmap(ProcessURL),
+        trace('Converted to List..............'), s2List(linebreak))(input)
+
+const inputFile = process.argv[2]
+const outputFile = process.argv[3]
+
+// Main pipeline.
 $M(
-    trace('Write to output file................'), FileWrite(utf8)(outputFile),
-    trace('Convert List 2 String...............'), l2String(newline),
-    trace('Waited..............................'), Wait,
-    trace('Processed URLs......................'), lmap(processURL),
-    trace('Converted to List...................'), s2List(linebreak),
-    trace('Read input file.....................'), FileRead(utf8))(inputFile)
-.catch($(exit, print))
-
-
-const _ = a => b => a+b
-print(_(1)(2))
+    trace('Wrote to output file................'), FileWrite(utf8)(outputFile),
+    trace('Processed content...................'), ProcessContent,
+    trace('Read input file.....................'), FileRead(utf8)
+)(inputFile)
+.catch(print)

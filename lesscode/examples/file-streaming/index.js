c0110925df32604f6d319461a3edb5a3d487b1c0
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-10-05 14:29:15 -0700

    applicative not supports both List and objects

--- lesscode/examples/file-streaming/index.js
+++ lesscode/examples/file-streaming/index.js
@@ -1,12 +1,17 @@
 const { 
     utf8, 
     suppercase,
-    $3, 
+    $3, M, 
     FileStreamIn, FileStreamOut,
 } = require('lesscode-fp')
 
 const is = FileStreamIn(utf8)(process.argv[2])
 const os = FileStreamOut(utf8)(process.argv[3])
 
-// Streaming pipeline. Classic example of doing pure function composition inside 2 im-pure (Monads), which also happens to be stream.
+/**
+Streaming pipeline. 
+Classic example of doing pure function composition inside 2 im-pure (Monads),
+which also happens to be stream.
+Stream, also allows monadic composition, since it converts pure to moand, internally.
+**/
 $3(os)(suppercase)(is)
